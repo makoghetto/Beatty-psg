c3984f993545d6eb378b0d5f950488347aeeb520
makoghetto <makoghetto@gmail.com>
2026-08-12 23:20:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js
 const API_URL = "https://script.google.com/macros/s/AKfycbxeopvqTCJbYXO0MpjsGtctqnXe4AHPHKuoYUyUUUy2jBKrWSiolaOQ_NldkWhjdtR_/exec";
+const PHOTO_QUALITY = 0.6; // Adjust compression quality here (e.g., 0.5 for smaller/faster, 0.8 for higher quality)
