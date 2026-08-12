24a9fc497bda5d7f3fe93cd675929424f0c77f61
makoghetto <makoghetto@gmail.com>
2026-08-12 21:37:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const API_URL = "https://script.google.com/macros/s/AKfycbzVctInXi1IR07byorWTtIVOVZsxGUn9LlG9OEKXTVw8r-ui42ki93EiMf6kz1Rdxsa/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbywnBSe3OCtyTreYHD3j2SCizzuwoToGPM9PJwq6OTvU-jmvGKdlIzHdvN3j5XSBgJg/exec";
